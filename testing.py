ef439d8326ef5642d35cb5409cfeebd12a57ac7b
UdarGIT829 <viraatudar9@gmail.com>
2023-04-25 12:53:46 -0700

    Delete testing.py

--- testing.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-thisList = [0.070191,2.171684,1.104309,0.005645,10.000000]
-
-for i,d in enumerate(thisList):
-    y = thisList[d]
-    x = np.random.normal(i + 1, 0.04, len(y))
-    print(x,y)
